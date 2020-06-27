512699ed4b7cdc189b6e3f477e867008539f77db
موسیقار پروگرامر <thesellersab@gmail.com>
2020-06-26 21:55:12 -0700

    Adding metadata for eclipse projects

---
+++ eclipseWorkSpaces/crackingCodingInterview/.metadata/.plugins/org.brainwy.liclipsetext.editor/ctags_gen/ctags_generation_temp_3.cpp
@@ -0,0 +1,32 @@
+/*
+
+*     *     *     *     *     *     *     *     *     *
+	*     *     *     *     *     *     *     *     *
+
+                   || ????:??????? ???????? ||
+
+    *     *     *     *     *     *     *     *     *
+*      *     *     *     *     *     *     *     *     *
+
+/*
+ * test_selector.hpp
+ *
+ *  Created on: Jun 10, 2020
+ *      Author: ??????? ???????? (Programming Musician)
+ */
+
+/*
+ * test_selector.hpp
+ *
+ *  Created on: Jun 11, 2020
+ *      Author: shahzadb
+ */
+
+#ifndef SRC_TEST_SELECTOR_HPP_
+#define SRC_TEST_SELECTOR_HPP_
+
+
+
+
+
+#endif /* SRC_TEST_SELECTOR_HPP_ */
---
+++ eclipseWorkSpaces/crackingCodingInterview/.metadata/.plugins/org.brainwy.liclipsetext.editor/ctags_gen/ctags_generation_temp_7.cpp
@@ -0,0 +1,29 @@
+/*
+
+*     *     *     *     *     *     *     *     *     *
+	*     *     *     *     *     *     *     *     *
+
+                   || ????:??????? ???????? ||
+
+    *     *     *     *     *     *     *     *     *
+*      *     *     *     *     *     *     *     *     *
+
+/*
+ * test_selector.hpp
+ *
+ *  Created on: Jun 10, 2020
+ *      Author: ??????? ???????? (Programming Musician)
+ */
+
+
+#ifndef SRC_TEST_SELECTOR_HPP_
+#define SRC_TEST_SELECTOR_HPP_
+
+// define only one constant 
+
+
+#define PALINDROME_STRING_TEST
+
+
+
+#endif /* SRC_TEST_SELECTOR_HPP_ */
